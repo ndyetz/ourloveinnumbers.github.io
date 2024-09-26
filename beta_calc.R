4db09976312558e515eb5df37a6de8a2beb7e3b4
ndyetz <ndyetz@gmail.com>
2024-09-26 14:55:30 -0700

    Clean up Beta.R Code and add notes

--- beta_calc.R
+++ beta_calc.R
@@ -1,6 +1,10 @@
+#This program was created on 9/26/24 by Neil Yetz
+#The purpose of this program is to calculate the beta values for a given portfolio over a specified time compared to an index (I.E. S&P 500)
+#The end product is an excel file with betas of all stocks indicated in "symbols" and corresponding fit statistics.
+# I mimicked code from this helpful guide: https://rviews.rstudio.com/2018/02/08/capm-beta/  
+
 #Clear environment
 rm(list = ls())
-
 
 #Load Libraries
 library(tidyquant)
@@ -8,24 +12,38 @@ library(tidyverse)
 library(timetk)
 library(tibbletime)
 library(broom)
+library(writexl)
 
 
-#Our "Portfolio"
+
+#Add stock ticker names. Ticker name must be the EXACT name (case sensitive)
 symbols <- c("SPY", "AMZN", "ABNB", "UBER","CMG", "ALK", "MODG", "GOLF", "MTN" , "SBUX")
 
+# Descriptive names of portfolio - must match order of symbols
+Asset_name <- as_tibble(c("S&P 500", "Amazon", "AirBnB", "Uber", "Chipotle", "Alaska Airlines", "Callaway", "Titleist", "Vail Resorts","Starbucks"))
+Asset_name <- Asset_name %>% rename(`Asset Name` = "value")
+
+
+#Start and end dates of interest, must be in "YYYY-MM-DD" format
+start_date <- "2022-06-27"
+end_date   <- "2024-09-23"
+
+
+#Pull Prices
 prices <- 
   getSymbols(symbols, src = 'yahoo', 
-             from = "2022-06-27" ,
-             to = "2024-09-23",
+             from = start_date ,
+             to = end_date,
              auto.assign = TRUE, warnings = FALSE) %>% 
-  map(~Ad(get(.))) %>%
+  map(~Ad(get(.))) %>%                          # <- Currently getting the adjusted closing price... Would "Close" (Cl) be better?
   reduce(merge) %>% 
   `colnames<-`(symbols)
 
 
-
+# Set to monthly... I odn't think we neeed to do it monthly, I am only doing that because the example was that way.
 prices_monthly <- to.monthly(prices, indexAt = "last", OHLC = FALSE)
 
+#Return calculator function (Bit of a black box to me, Karina can help?)
 asset_returns_xts <- na.omit(Return.calculate(prices_monthly, method = "log"))
 
 portfolio_returns_xts_rebalanced_monthly <- 
@@ -33,6 +51,7 @@ portfolio_returns_xts_rebalanced_monthly <-
   `colnames<-`("returns") 
 
 
+# Getting indexes at monthly and Returns = log(returns) - log(lag(returns)) lag is currently lagging by 1 (deafult). Ask Karina if this makes sense?
 asset_returns_long <-  
   prices %>% 
   to.monthly(indexAt = "last", OHLC = FALSE) %>% 
@@ -42,6 +61,7 @@ asset_returns_long <-
   mutate(returns = (log(returns) - log(lag(returns)))) %>% 
   na.omit()
 
+# Aggregate the group of returns by asset
 portfolio_returns_tq_rebalanced_monthly <- 
   asset_returns_long %>%
   tq_portfolio(assets_col  = asset, 
@@ -50,17 +70,15 @@ portfolio_returns_tq_rebalanced_monthly <-
                rebalance_on = "months")
 
 
-
-
-#SP500
+#SP500 Index comparison
 spy_monthly_xts <- 
   getSymbols("SPY", 
              src = 'yahoo', 
-             from = "2022-06-27" ,
-             to = "2024-09-23",
+             from = start_date ,
+             to = end_date,
              auto.assign = TRUE, 
              warnings = FALSE) %>% 
-  map(~Ad(get(.))) %>% 
+  map(~Ad(get(.))) %>%                                   # <- Currently getting the adjusted closing price... Would "Close" (Cl) be better?
   reduce(merge) %>%
   `colnames<-`("SPY") %>% 
   to.monthly(indexAt = "last", OHLC = FALSE)
@@ -82,35 +100,11 @@ market_returns_tidy <-
 #  head()
 
 
-cov(portfolio_returns_xts_rebalanced_monthly,market_returns_tidy$returns)/var(market_returns_tidy$returns)
+#Get overall portfolio beta
+#cov(portfolio_returns_xts_rebalanced_monthly,market_returns_tidy$returns)/var(market_returns_tidy$returns)
 
 
-
-beta_assets <- 
-  asset_returns_long %>%
-  na.omit() %>% 
-  nest(data = -asset)
-
-
-
-
-beta_assets <- 
-  asset_returns_long %>% 
-  na.omit() %>% 
-  nest(data = -asset) %>% 
-  mutate(model = map(data, ~ lm(returns ~ market_returns_tidy$returns, data = .))) 
-
-
-
-beta_assets <- 
-  asset_returns_long %>% 
-  na.omit() %>% 
-  nest(data = -asset) %>% 
-  mutate(model = map(data, ~ lm(returns ~ market_returns_tidy$returns, data = .))) %>%
-  mutate(model = map(model, tidy))
-
-
-
+#Run models, tidy betas, keep fit statistics
 beta_assets <- 
   asset_returns_long %>% 
   na.omit() %>% 
@@ -121,36 +115,15 @@ beta_assets <-
   filter(term == "market_returns_tidy$returns") %>% 
   select(-data)
 
+#Adds in descriptive names of tickers
+beta_assets <- bind_cols(Asset_name, beta_assets) 
 
-`Asset_name` <- as_tibble(
-                  c("S&P 500",
-                   "Amazon",
-                   "AirBnB",
-                   "Uber",
-                   "Chipotle",
-                   "Alaska Airlines",
-                   "Callaway",
-                   "Titleist",
-                  "Vail Resorts",
-                  "Starbucks")
-                  )
-
-`Asset_name` <- Asset_name %>% 
-  rename(`Asset Name` = "value")
-
-beta_assets <- bind_cols(`Asset_name`, beta_assets)
-
+#Format final dataset
 final_betas <- beta_assets %>% 
   mutate(term = "Market Returns",
-         Dates = "2022-06-27 - 2024-09-23") %>% 
+         Dates = paste(start_date, "-", end_date)) %>% 
   select(`Asset Name`, Ticker = "asset", term, Dates, `Beta` = "estimate", "Standard Error" = std.error, `t-value` = "statistic", "p-value" = p.value)
-  
-  
 
 
-library(writexl)
-
-
+#Output Betas to excel
 write_xlsx(final_betas, "D:/Git/ourloveinnumbers.github.io/final_betas.xlsx")
-
-
